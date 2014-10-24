826bcc77e273546924555a691f0e645a20b0c2ff
bus-5170 <bus-5170@vm5170-14.business.utah.edu>
2014-10-23 19:34:48 -0600

    Initial Commit 2

---
+++ src/bicyclerentalmgrclass/BikeType.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bicyclerentalmgrclass;
+
+/**
+ *
+ * @author bus-5170
+ */
+public enum BikeType {
+    
+}
--- src/bicyclerentalmgrclass/Renter.java
+++ src/bicyclerentalmgrclass/Renter.java
@@ -11,6 +11,18 @@ package bicyclerentalmgrclass;
  * @author bus-5170
  */
 public class Renter {
+    //make your fields privates and your methods public
+    //create field for renter's ID
+    private int rID;
+    //created field for hours requested for renter
+    private int hoursRequested;
     
+    //This seems like a method name Renter is the same as the class name
+    //If I take out public then the package name would be Renter; this is default
+    //By making it public any file can access this file with the method called Renter
+    //Constructer
+    public Renter (int id, int time) {
+        
+    }
     
 }
