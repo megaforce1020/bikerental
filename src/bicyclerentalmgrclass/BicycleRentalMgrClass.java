26b3d2a71170968054fa6a2dd9d88b63f57772ff
bus-5170 <bus-5170@vm5170-14.business.utah.edu>
2014-10-23 21:00:15 -0600

    Create objects

--- src/bicyclerentalmgrclass/BicycleRentalMgrClass.java
+++ src/bicyclerentalmgrclass/BicycleRentalMgrClass.java
@@ -6,6 +6,8 @@
 
 package bicyclerentalmgrclass;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author bus-5170
@@ -16,7 +18,25 @@ public class BicycleRentalMgrClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //defined an object called b
+        Bicycle b;
+        ArrayList<Bicycle> theBikes = new ArrayList<>();
+        //created two objects, gave them an ID and identified what bike type
+        //the ID are for
+        b = new Bicycle(101,BikeType.mountainbike);
+        //now add the variable b to the ArrayList
+        theBikes.add(b);
+        //now I can use the memory spot b again
+        //double referencing
+        b = new Bicycle(102,BikeType.roadbike);
+        theBikes.add(b);
+        
+        Renter r1 = new Renter(101, 6);
+        
+        
+        
+        
+        
     }
     
 }
