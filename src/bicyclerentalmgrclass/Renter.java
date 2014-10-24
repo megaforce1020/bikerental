732cdb969e562acbae35c8ed8e0b1b7c10291e14
bus-5170 <bus-5170@vm5170-14.business.utah.edu>
2014-10-23 19:47:47 -0600

    adding Constructors and local variables

--- src/bicyclerentalmgrclass/Renter.java
+++ src/bicyclerentalmgrclass/Renter.java
@@ -20,8 +20,22 @@ public class Renter {
     //This seems like a method name Renter is the same as the class name
     //If I take out public then the package name would be Renter; this is default
     //By making it public any file can access this file with the method called Renter
-    //Constructer
+    //Constructor Renter with defined local variables
     public Renter (int id, int time) {
+        //in these constructer lines of code you are matching the fields with the local variables stored in memory
+        //this means that someone has given you an input
+        //the order is very important
+        rID=id;
+        hoursRequested=time;
+    }
+    public Renter(){
+        //if there is no input then you should do this
+        //no-input constructor meaning there is no local variables defined
+        //if you don't write rID=0 it will do so by default anyways
+        rID=0;
+        hoursRequested=0;
+    }
+    public Renter(int int1){
         
     }
     
